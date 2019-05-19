6045f034b28c33a67a5699a705e0326a89bab5a7
LYF <2498190377@qq.com>
2019-05-19 22:06:05 +0800

    将统一验证改为每个路由验证

--- controller/usersContr.js
+++ controller/usersContr.js
@@ -4,20 +4,28 @@ const userdb = require('../model/userdb.js')
 module.exports = {
     // 1.0   处理所有与 用户 相关的逻辑
     getUsers: (req, res) => {
-        // 将所有的用户数据查询出来
-        userdb.query('SELECT * FROM users', (err, result) => {
-            // 渲染页面, 渲染数据
-            res.render('users', { result: result })
-        })
+           if (isBroLogin(req, res)) {
+               return  // 如果不是，不继续执行下面代码
+           }
+            userdb.query('SELECT * FROM users', (err, result) => {
+                if (err) {
+                    return res.send(`<script>alert(${err.message})</script>`)
+                }
+                // 渲染页面 ，渲染数据
+                res.render('users', {result: result})
+            })
     },
     //2.0  添加用户数据
     addUsers: (req, res) => {
+        if (isXHRLogin(req, res)) {
+            return
+        }
         //接收提交的参数
         var params = req.body
-        if (params.slug == '' ||  params.email == '' || params.password == '') {
+        if (params.slug == '' || params.email == '' || params.password == '') {
             return res.send({
-                    status: 400,
-                    msg: '添加失败: 邮箱 ， 密码，用户名必填'
+                status: 400,
+                msg: '添加失败: 邮箱 ， 密码，用户名必填'
             })
         }
         // 2.0 将数据提交到数据库
@@ -39,6 +47,9 @@ module.exports = {
     },
     //3.0 处理获取所有用户的请求
     getAllUsers: (req, res) => {
+        if (isXHRLogin(req, res)) {
+            return
+        }
         userdb.query(`SELECT * FROM users`, (err, result) => {
             if (err) {
                 res.send({
@@ -57,6 +68,9 @@ module.exports = {
 
     //4.0 处理删除用户的路由
     delUser: (req, res) => {
+        if (isXHRLogin(req, res)) {
+            return
+        }
         //获取get请求的参数 id
         let id = req.query.id
         //console.log(id)
@@ -77,6 +91,9 @@ module.exports = {
 
     //5.0根据id响应数据
     getIdData: (req, res) => {
+        if (isXHRLogin(req, res)) {
+            return
+        }
         //获取id
         let id = req.query.id
         // console.log(id)
@@ -101,6 +118,9 @@ module.exports = {
 
     //6.0 处理修改后的表单提交数据
     editPost: (req, res) => {
+        if (isXHRLogin(req, res)) {
+            return
+        }
         //获取参数
         let params = req.body
         // console.log(params)
@@ -122,6 +142,9 @@ module.exports = {
 
     //7.0 处理批量删除的逻辑
     delUsers: (req, res) => {
+        if (isXHRLogin(req, res)) {
+            return
+        }
         //获取post提交的参数
         let params = req.body
         // console.log(params) //{ id: [ '1', '2', '36' ] }
@@ -146,4 +169,24 @@ module.exports = {
 
         })
     }
+}
+
+// 封装一个函数，验证是否登录
+function isXHRLogin(req, res) {
+    if (!req.session.user) {
+        res.send({
+            status: 304,
+            msg: '还没有登录'
+        })
+        return true
+    }
+    return false
+}
+
+function isBroLogin(req, res) {
+    if (!req.session.user) {
+       res.send(`<script>alert('你还没有登录');window.location='/login'</script>`)
+       return true
+    } 
+    return false
 }
